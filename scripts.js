8b5724c35b571626cf2dc97c58a74e82b1e4e76e
LukaszWiszniewski <wisznu07@gmail.com>
2017-12-06 18:09:29 +0100

    Added shape og HTML

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-var newGameBtn = document.getElementById('js-newGameElement');
+var newGameBtn = document.getElementById('js-newGameElement'),
+    pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors');
 
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
 function newGame () {
 
 }
